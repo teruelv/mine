d6da144d95b6f183294f0b00a4d8e19f7798d2d6
teruelv <89648902+teruelv@users.noreply.github.com>
2021-08-29 04:45:39 -0700

    Update ST persistente O(NlogN).cpp

--- Data structures/ST persistente O(NlogN).cpp
+++ Data structures/ST persistente O(NlogN).cpp
@@ -1,69 +1,62 @@
 struct STP { // persistent segment tree
-    vector <int> st, l, r, rt;
-    int N;
+    struct node {
+        int v, l, r;
+        node(int a = 0, int b = 0, int c = 0) : v(a), l(b), r(c) {}
+    } ;
+    vector <node> st;
+    vector <int> rt;
+    int N, sz;
     STP(int size) {
-        N = size;
+        N = size, sz = 0;
         rt.push_back(0);
         build(1, N);
     }
     inline void push() {
-        l.push_back(0);
-        r.push_back(0);
-        st.push_back(0);
+        sz++;
+        st.push_back(node(0, 0, 0));
     }
-    int build(int L, int R) {
-        int p = size();
+    void build(int L, int R) {
+        int p = sz;
         push();
         if(L != R) {
             int m = (L + R) >> 1;
-            l[p] = size();
+            st[p].l = sz;
             build(L, m);
-            r[p] = size();
+            st[p].r = sz;
             build(m + 1, R);
         }
-        return size() - 1;
     }
     void copy(int x, int y) {
         st[y] = st[x];
-        l[y] = l[x];
-        r[y] = r[x];
     }
     void update(int p, int x, int L, int R, int v) {
         push();
         copy(p, x);
-        st[x]++;
+        st[x].v++;
         if(L == R)return;
         int m = (L + R) >> 1;
         if(v <= m)
-            update(l[p], l[x] = size(), L, m, v);
+            update(st[p].l, st[x].l = sz, L, m, v);
         else
-            update(r[p], r[x] = size(), m + 1, R, v);
+            update(st[p].r, st[x].r = sz, m + 1, R, v);
     }
-    int query(int p, int x, int L, int R, int v) {   // freq(v)
-        if(L == R)
-            return st[x] - st[p];
-        int m = (L + R) >> 1;
-        if(v <= m)
-            return query(l[p], l[x], L, m, v);
-        return query(r[p], r[x], m + 1, R, v);
-    }
-    int Kth(int p, int x, int L, int R, int kth) {
+    int query(int p, int x, int L, int R, int kth) {
         if(L == R)return L;
         int m = (L + R) >> 1;
-        int izq = st[l[x]] - st[l[p]];
+        int izq = st[st[x].l].v - st[st[p].l].v;
         if(izq >= kth)
-            return Kth(l[p], l[x], L, m, kth);
-        return Kth(r[p], r[x], m + 1, R, kth - izq);
+            return query(st[p].l, st[x].l, L, m, kth);
+        return query(st[p].r, st[x].r, m + 1, R, kth - izq);
     }
     int ask(int pre, int nxt, int v) {
         return query(rt[pre - 1], rt[nxt], 1, N, v);
     }
     void add(int v) {
         int tam = rt.size();
-        rt.push_back(size());
+        rt.push_back(sz);
         update(rt[tam - 1], rt[tam], 1, N, v);
     }
     inline int size() {
-        return st.size();
+        return sz;
     }
 } ;
