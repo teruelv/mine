993dd35d9f345a02e684df1cc6b3eae367fa2f96
teruelv <89648902+teruelv@users.noreply.github.com>
2021-08-29 05:07:36 -0700

    Update Builtin stuff.cpp

--- Bitmasks/Builtin stuff.cpp
+++ Bitmasks/Builtin stuff.cpp
@@ -3,3 +3,5 @@
 __builtin_popcount(n); // # de bits activos
 __builtin_parity(n); // count odd?
 __builtin_clz(n); // counts leading zeroes clz(16) = 27, log2(x) = 31 - clz(x)
+__builtin_ctz(n); // counts trailing zeroes
+
