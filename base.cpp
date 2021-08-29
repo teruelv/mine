00004d31531f30976e66baed8a6783c5eb8fefea
teruelv <89648902+teruelv@users.noreply.github.com>
2021-08-29 05:01:28 -0700

    Create base.cpp

---
+++ base.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#ifdef ONLINE_JUDGE
+//#pragma GCC optimize("Ofast","unroll-loops","omit-frame-poller","inline","03")
+#endif
+
+#define all(x) (x).begin(), (x).end()
+#define uni(x) (x).erase(unique(all(x)), (x).end())
+#define rnk(x, y) upper_bound(all((x)), (y)) - (x).begin()
+
+typedef long double ld;
+typedef long long ll;
+typedef pair <int, int> ii;
+typedef __int128 LL;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+}
