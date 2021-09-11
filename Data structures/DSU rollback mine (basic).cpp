eb86c0d4423d37d7f767bd190574983900c026f8
teruelv <89648902+teruelv@users.noreply.github.com>
2021-09-11 13:10:20 -0700

    Update DSU rollback mine (basic).cpp

--- Data structures/DSU rollback mine (basic).cpp
+++ Data structures/DSU rollback mine (basic).cpp
@@ -6,7 +6,7 @@ struct DSU_ROLLBACK {
     int n, comps;
     vector <node> ds;
     stack <node> s;
-    DSU_ROLLBACK(int sz = 0) {
+    DSU_ROLLBACK(int sz) {
         n = comps = sz;
         ds.resize(n);
         for(int i = 0; i < n; i++)ds[i] = node(i, i);
@@ -28,13 +28,18 @@ struct DSU_ROLLBACK {
         ds[a].p = b;
         ds[b].sz += ds[a].sz;
     }
-    void panic() {
-        while(s.size()) {
+    void persist() {
+        s.push(node(-1, -1, -1));
+    }
+    void rollback() {
+        while(s.size() && s.top().id != -1) {
             comps++;
             node x = s.top();
             ds[x.id] = x, s.pop();
             node y = s.top();
             ds[y.id] = y, s.pop();
         }
+        if(s.size() && s.top().id == -1)
+            s.pop();
     }
 } ;
