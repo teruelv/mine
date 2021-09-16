75bb0a9552c319ec73344fd046eeb3c7d42a9104
teruelv <89648902+teruelv@users.noreply.github.com>
2021-09-16 12:18:21 -0700

    Create knapsack O(sqrtK logK).cpp

---
+++ Classic problems/knapsack O(sqrtK logK).cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+template <class T> using Tree = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+#ifdef ONLINE_JUDGE
+#pragma GCC optimize("Ofast","unroint-loops","omit-frame-pointer","inline","03")
+#endif // ONLINE_JUDGE
+
+#define all(x) (x).begin(), (x).end()
+#define uni(x) (x).erase(unique(all(x)), (x).end())
+#define rnk(x, y) upper_bound(all((x)), (y)) - (x).begin()
+
+mt19937 rng(chrono::high_resolution_clock::now().time_since_epoch().count());
+
+static int rnd(int lo, int hi) {
+    return uniform_int_distribution <int> (lo, hi)(rng);
+}
+
+typedef long double ld;
+typedef long long ll;
+typedef pair <int, int> ii;
+typedef __int128 LL;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n, k;
+
+    cin >> n >> k;
+
+    map <int, int> m;
+
+    while(n--) {
+        int x;
+        cin >> x;
+        m[x]++;
+    }
+
+    vector <ii> v;
+    for(auto [x, y] : m) {
+        v.push_back(ii(x, y));
+    }
+    m.clear();
+
+    bitset <200000> dp;
+    dp[0] = 1;
+
+    for(auto [x, y] : v) {
+        if(y == 1) {
+            dp |= dp << x;
+        } else {
+            int lg = __lg((y + 1) / 2);
+            int s = (1 << (lg + 1)) - 1;
+            dp |= dp << (x * (y - s));
+            lg = 1 << lg;
+            while(lg) {
+                dp |= dp << (x * lg);
+                lg >>= 1;
+            }
+        }
+        if(dp[k]) {
+            cout << "YES\n";
+            return 0;
+        }
+    }
+
+    cout << "NO\n";
+    return 0;
+}
