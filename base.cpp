1ef610d3d77defa0a8d7a9b06179fe8e43068417
teruelv <89648902+teruelv@users.noreply.github.com>
2021-12-10 11:26:11 -0500

    Update base.cpp

--- base.cpp
+++ base.cpp
@@ -12,24 +12,16 @@ template <class T> using Tree = tree<T, null_type, less<T>, rb_tree_tag, tree_or
 #endif // ONLINE_JUDGE
 
 #define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
 #define uni(x) (x).erase(unique(all(x)), (x).end())
 #define rnk(x, y) upper_bound(all((x)), (y)) - (x).begin()
 
+template <class T = int> using ii = pair <T, T>;
+template <class T = int> using tri = tuple <T, T, T>;
+
 typedef long double ld;
 typedef long long ll;
-typedef pair <int, int> ii;
 typedef __int128 LL;
-
-template <typename T> T pot(T b, ll e) {
-    T ret = 1;
-    while(e) {
-        if(e & 1)
-            ret = ret * b;
-        b = b * b;
-        e /= 2;
-    }
-    return ret;
-}
 
 mt19937 rng(chrono::high_resolution_clock::now().time_since_epoch().count());
 
@@ -37,9 +29,12 @@ static int rnd(int lo, int hi) {
     return uniform_int_distribution <int> (lo, hi)(rng);
 }
 
+const ll oo = 1e18;
+
+const ll MAX = 2e5 + 5;
+const ll mod = 1e9 + 7;
+
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-
-    ;
 }
