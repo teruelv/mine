87881572231ce215412fc02d5a51f2587452d07e
teruelv <89648902+teruelv@users.noreply.github.com>
2023-11-06 23:46:43 -0400

    Update base.cpp

--- base.cpp
+++ base.cpp
@@ -31,7 +31,15 @@ const ll oo = 1e18;
 const ll MAX = 2e5 + 5;
 const ll mod = 1e9 + 7;
 
+void solve() {
+    ;
+}
+
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
+
+    int tc = 1;
+    while(tc--)
+        solve();
 }
